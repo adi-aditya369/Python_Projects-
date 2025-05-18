c2d8697eeb21072cc3db481fb29b9726dd90c50f
aditya <adi199696adi199696@gmail.com>
2025-05-18 14:23:54 +0530

    mab

---
+++ mab_ibs.py
@@ -0,0 +1,21 @@
+# name = input("enter your name")
+# age = int(input("enter your age"))
+# age = age + 1
+# print(f"hello {name}")
+# print(f"you are {age} years old")
+
+word1 =input("enter your word")
+word2 =input("enter your word")
+word3 =input("enter your word")
+word4 =input("enter your word")
+word5 =input("enter your word")
+
+
+print(f"Some feelings are {word1}, some feelings are {word2}.")
+print(f"Some make us {word3}, some make us {word4}.")
+print(f"Some we {word5}, some we don't.")
+
+# Some feelings are shallow, some feelings are deep.
+# Some make us smile, some make us weep.
+# Some we love, some we don't.
+
