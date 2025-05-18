1f22440d8b842819047db21e17e48ec42a17d328
aditya <adi199696adi199696@gmail.com>
2025-05-18 14:22:16 +0530

    if else

---
+++ if_else.py
@@ -0,0 +1,18 @@
+# if = do some code only if some condition is true 
+#  else do something else 
+
+# age = int(input("enter your age: "))
+
+# if age >=18:
+#     print("you are now signed up !")
+# elif age <= 100:
+#     print("you are to old to sign up ")
+# else:
+#     print("you must be 18+ to sign up !")
+
+for_sale =False
+if for_sale:
+    print("this item is for sale")
+else:
+    print("this item is not for sale")    
+       
